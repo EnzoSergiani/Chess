af0967df1b2f7e3c92a639c571f57b70d846ae3e
EnzoSergiani <EnzoSergiani@users.noreply.github.com>
2023-11-18 21:44:22 +0000

    deploy: c01759ff3a02c395ec4306fc59da54c3d3e2973a

--- search/searchdata.js
+++ search/searchdata.js
@@ -1,21 +1,30 @@
 var indexSectionsWithContent =
 {
-  0: "cfm",
-  1: "cm",
-  2: "cfm"
+  0: "cfmpst",
+  1: "p",
+  2: "cm",
+  3: "cfm",
+  4: "ct",
+  5: "s"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "files",
-  2: "functions"
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "enums",
+  5: "defines"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Files",
-  2: "Functions"
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Enumerations",
+  5: "Macros"
 };
 
