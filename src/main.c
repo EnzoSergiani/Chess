7185aa25cdfbb9c3faf17c7449616ed7044e4b69
Enzo Sergiani <enzo.sergiani@student.junia.com>
2023-11-18 21:59:00 +0100

    Merge

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/**
+ * \file main.c
+ * \brief Main program file for the chess application.
+ * \author Enzo SERGIANI
+ */
+
+#include "../inc/chess.h"
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  chess();
+  return EXIT_SUCCESS;
+}
