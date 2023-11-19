be66a13a6171bb106ae22b36f36860ed12a39774
EnzoSergiani <EnzoSergiani@users.noreply.github.com>
2023-11-19 12:26:31 +0000

    deploy: de7f19f4c9a3d27fc7e8c5f913150d6263992832

---
+++ search/files_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['chess_2ec_0',['chess.c',['../chess_8c.html',1,'']]],
+  ['chess_2eh_1',['chess.h',['../chess_8h.html',1,'']]]
+];
---
+++ search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['freeboard_0',['freeboard',['../chess_8c.html#a71649c93b8ebed8710777e0ae8bbe478',1,'freeBoard(Piece **board):&#160;chess.c'],['../chess_8h.html#a71649c93b8ebed8710777e0ae8bbe478',1,'freeBoard(Piece **board):&#160;chess.c']]]
+];
