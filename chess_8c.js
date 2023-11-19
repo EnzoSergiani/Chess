e8aece023fd14991a2580734b108e4f01a22c87b
EnzoSergiani <EnzoSergiani@users.noreply.github.com>
2023-11-19 09:02:27 +0000

    deploy: 4f85405400a07a9e8b6fd36cdb4bf63ce2cf1f58

---
+++ chess_8c.js
@@ -0,0 +1,6 @@
+var chess_8c =
+[
+    [ "chess", "chess_8c.html#a9756db40a56ebd7b53ee6a5db3e5c442", null ],
+    [ "freeBoard", "chess_8c.html#a71649c93b8ebed8710777e0ae8bbe478", null ],
+    [ "mallocBoard", "chess_8c.html#a06b81730736f82f2f130bc5ae4e3e1e9", null ]
+];
