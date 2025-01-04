22f40e3d54a65a61b4d0f1ef3b95ae56899c17f1
Enzo Sergiani <enzo.sergiani@protonmail.com>
2025-01-04 15:53:09 +0100

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod board;
 mod piece;
+mod shift;
 
 use crate::board::Board;
 
@@ -7,12 +8,31 @@ use yew::prelude::*;
 
 #[function_component(App)]
 fn app() -> Html {
-    let mut board = Board::new();
-    board.initialize();
+    let board: UseStateHandle<Board> = use_state(|| Board::new());
+
+    {
+        let board: UseStateHandle<Board> = board.clone();
+        use_effect_with_deps(
+            move |_| {
+                board.set(Board::new().initialize());
+                || ()
+            },
+            (),
+        );
+    }
+
+    let on_click: Callback<(usize, usize)> = {
+        let board: UseStateHandle<Board> = board.clone();
+        Callback::from(move |(row, col): (usize, usize)| {
+            let mut new_board = (*board).clone();
+            new_board.handle_click(row, col);
+            board.set(new_board);
+        })
+    };
 
     html! {
         <div>
-            {board.render()}
+            {board.render(on_click)}
         </div>
     }
 }
